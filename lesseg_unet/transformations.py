94c975355261c4bab580fc235dcb1e8f9ddcd2dc
chrisfoulon <tolhsadum@gmail.com>
2022-03-14 20:01:15 +0000

    print

--- lesseg_unet/transformations.py
+++ lesseg_unet/transformations.py
@@ -269,6 +269,7 @@ class CoordConvd(MapTransform, InvertibleTransform):
             # Apply inverse transform
             d[key] = d[key][:1, :, :, :]
             if isinstance(d[key], torch.Tensor):
+                print('Tensor to numpy')
                 d[key] = d[key].cpu().detach().numpy()
             # Remove the applied transform
             self.pop_transform(d, key)
