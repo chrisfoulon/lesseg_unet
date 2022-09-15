66eed63ff5f38a568acd06044fc1ca5949e68227
chrisfoulon <tolhsadum@gmail.com>
2022-09-15 17:11:34 +0100

    trying to find out what the problem is

--- lesseg_unet/utils.py
+++ lesseg_unet/utils.py
@@ -459,8 +459,15 @@ def compare_img_to_cluster(img, cluster, comp_meth='dice', cluster_thr=None, fli
     if cluster_thr is not None and len(np.unique(cluster_data)) > 2:
         cluster_data[cluster_data < cluster_thr] = 0
         cluster_data[cluster_data >= cluster_thr] = 1
-    img_data = torch.tensor(np.ndarray([img_data]))
-    cluster_data = torch.tensor([cluster_data])
+    print(type(img_data))
+    print(img_data.dtype)
+    try:
+        img_data = torch.tensor(np.ndarray([img_data]))
+        cluster_data = torch.tensor([cluster_data])
+    except TypeError as e:
+        print('type error')
+        img_data = np.array(np.ndarray([img_data]))
+        cluster_data = np.array([cluster_data])
     if comp_meth == 'distance':
         hausdorff_metric = HausdorffDistanceMetric(include_background=True, reduction="mean")
         return hausdorff_metric(y_pred=[img_data], y=[cluster_data]).item()
