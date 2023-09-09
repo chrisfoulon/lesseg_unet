74865c726205d643877fb38c32ca6edfa16c5c1d
chrisfoulon <tolhsadum@gmail.com>
2023-09-09 12:24:28 +0100

    typo

--- lesseg_unet/segmentation_noise_increment.sh
+++ lesseg_unet/segmentation_noise_increment.sh
@@ -8,7 +8,7 @@ do
   do
     for i in {0..4};
     do
-      lesseg_unet -o "/media/chrisfoulon/DATA1/a_imagepool_mr/noise_increment_experiment/${model_name})/${noise_type}_$((incr * 10))/fold_${i}" \
+      lesseg_unet -o "/media/chrisfoulon/DATA1/a_imagepool_mr/noise_increment_experiment/${model_name}/${noise_type}_$((incr * 10))/fold_${i}" \
       -li "/media/chrisfoulon/HDD2/final_training_set/split_lists_per_fold/fold_${i}_img_list.csv" \
       -lli "/media/chrisfoulon/HDD2/final_training_set/split_lists_per_fold/fold_${i}_lbl_list.csv" \
       -trs "destructive_${noise_type}_$((incr * 10))" -nw 3 -mt swin-unetr -overlap -sa \
