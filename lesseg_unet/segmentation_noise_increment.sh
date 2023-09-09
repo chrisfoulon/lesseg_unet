913c0b4a4c84142de5f1d9ca962dddf557d2c8aa
chrisfoulon <tolhsadum@gmail.com>
2023-09-09 12:18:26 +0100

    made it a bit more modular

--- lesseg_unet/segmentation_noise_increment.sh
+++ lesseg_unet/segmentation_noise_increment.sh
@@ -1,12 +1,14 @@
 checkpoint_root_path=$1
 
+model_name=$(basename "$checkpoint_root_path")
+
 for noise_type in "gibbs" "rician" "bias" "all_noises";
 do
   for incr in {0..10};
   do
     for i in {0..4};
     do
-      lesseg_unet -o "/media/chrisfoulon/DATA1/a_imagepool_mr/noise_increment_experiment/noise_increment/${noise_type}_$((incr * 10))/fold_${i}" \
+      lesseg_unet -o "/media/chrisfoulon/DATA1/a_imagepool_mr/noise_increment_experiment/${model_name})/${noise_type}_$((incr * 10))/fold_${i}" \
       -li "/media/chrisfoulon/HDD2/final_training_set/split_lists_per_fold/fold_${i}_img_list.csv" \
       -lli "/media/chrisfoulon/HDD2/final_training_set/split_lists_per_fold/fold_${i}_lbl_list.csv" \
       -trs "destructive_${noise_type}_$((incr * 10))" -nw 3 -mt swin-unetr -overlap -sa \
