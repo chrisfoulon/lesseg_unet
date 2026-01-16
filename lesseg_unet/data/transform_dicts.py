51bab38fc677db7f493b049b4b19858a103c4916
chrisfoulon <tolhsadum@gmail.com>
2026-01-16 20:36:48 +0100

    trying to fit 2 batches on the GPU

--- lesseg_unet/data/transform_dicts.py
+++ lesseg_unet/data/transform_dicts.py
@@ -1784,8 +1784,8 @@ def create_multimodal_transform_dict(
                 'label_key': 'label',
                 'spatial_size': [patch_size, patch_size, patch_size],
                 'pos': 1,
-                'neg': 2,  # High negative sampling for artifact rejection
-                'num_samples': 3,
+                'neg': 1,  # High negative sampling for artifact rejection
+                'num_samples': 1,
             }},
         ],
     }
